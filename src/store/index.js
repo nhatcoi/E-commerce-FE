6b8364f57a55932edd3baa4da7dcde32db5f9e9b
nhatcoi <nhatcoifacebook@gmail.com>
2025-03-20 23:23:12 +0700

    fix(redux): store

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,8 @@ import ratingReducer from "./slices/product/ratingSlice.js";
 import categoriesReducer from "./slices/categoriesSlice.js";
 import blogReducer from "./slices/blogsSlice.js";
 import filtersReducer from "./slices/product/filtersSlice.js";
+import cartsReducer from "./slices/cart/cartSlice.js";
+import authReducer from "./slices/authSlice.js";
 
 const store = configureStore({
     reducer: {
@@ -12,6 +14,8 @@ const store = configureStore({
         categories: categoriesReducer,
         blogs: blogReducer,
         filters: filtersReducer,
+        cart: cartsReducer,
+        auth: authReducer,
     },
     // eslint-disable-next-line no-undef
     devTools: process.env.NODE_ENV !== "production",
