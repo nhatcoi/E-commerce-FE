d346d6efcf127d3435aeba250bbc88871aab07f3
nhatcoi <nhatcoifacebook@gmail.com>
2025-03-27 22:45:01 +0700

    feat(store): implement Redux Persist for auth state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import productReducer from "./slices/product/productsSlice.js";
 import ratingReducer from "./slices/product/ratingSlice.js";
 import categoriesReducer from "./slices/categoriesSlice.js";
@@ -7,18 +9,37 @@ import filtersReducer from "./slices/product/filtersSlice.js";
 import cartsReducer from "./slices/cart/cartSlice.js";
 import authReducer from "./slices/authSlice.js";
 
+// Configure Redux Persist
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['auth'], // only auth will be persisted
+};
+
+const rootReducer = combineReducers({
+    products: productReducer,
+    ratings: ratingReducer,
+    categories: categoriesReducer,
+    blogs: blogReducer,
+    filters: filtersReducer,
+    cart: cartsReducer,
+    auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: {
-        products: productReducer,
-        ratings: ratingReducer,
-        categories: categoriesReducer,
-        blogs: blogReducer,
-        filters: filtersReducer,
-        cart: cartsReducer,
-        auth: authReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                // Ignore these action types
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/REGISTER'],
+            },
+        }),
     // eslint-disable-next-line no-undef
     devTools: process.env.NODE_ENV !== "production",
 });
 
+export const persistor = persistStore(store);
 export default store;
