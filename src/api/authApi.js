c8c070ca3699ca542025c34dc0e2fb566574914c
nhatcoi <nhatcoifacebook@gmail.com>
2025-03-27 18:07:12 +0700

    feat(api): update authentication endpoints and add user registration

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,8 @@
 import api from "src/config/api";
 
 export const authApi = {
-    login: (email, password) => api.post("/login", { email, password }),
-    logout: () => api.post("/logout"),
-    refreshToken: () => api.post("/refresh-token"),
+    login: (userIdentifier, password) => api.post("/auth/log-in", { userIdentifier, password }),
+    logout: () => api.post("/auth/log-out"),
+    refreshToken: () => api.post("/auth/refresh-token"),
+    register: (userData) => api.post("/auth/register", userData),
 }
